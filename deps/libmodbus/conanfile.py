7804108a10d99fc50713e4d4edc41f04bc9a6aa5
Rainer Poisel <rainer.poisel@gmail.com>
2022-11-22 23:45:10 +0100

    feat: add (unmodified) libmodbus

---
+++ deps/libmodbus/conanfile.py
@@ -0,0 +1,137 @@
+from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.env import Environment, VirtualBuildEnv
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rename, replace_in_file, rm, rmdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
+from conan.tools.scm import Version
+import os
+
+
+required_conan_version = ">=1.52.0"
+
+
+class LibmodbusConan(ConanFile):
+    name = "libmodbus"
+    version = "3.1.8"
+    description = "libmodbus is a free software library to send/receive data according to the Modbus protocol"
+    homepage = "https://libmodbus.org/"
+    topics = ("modbus", "protocol", "industry", "automation")
+    license = "LGPL-2.1"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
+    @property
+    def _user_info_build(self):
+        return getattr(self, "user_info_build", self.deps_user_info)
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
+        try:
+            del self.settings.compiler.libcxx
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd
+        except Exception:
+            pass
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def build_requirements(self):
+        if is_msvc(self):
+            self.tool_requires("automake/1.16.5")
+        if self._settings_build.os == "Windows":
+            if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+                self.tool_requires("msys2/cci.latest")
+            self.win_bash = True
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version],
+                destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+
+        tc = AutotoolsToolchain(self)
+        if Version(self.version) < "3.1.8":
+            tc.configure_args.append("--without-documentation")
+        tc.configure_args.append("--disable-tests")
+        if (self.settings.compiler == "Visual Studio" and Version(self.settings.compiler.version) >= "12") or \
+           (self.settings.compiler == "msvc" and Version(self.settings.compiler.version) >= "180"):
+            tc.extra_cflags.append("-FS")
+        tc.generate()
+
+        if is_msvc(self):
+            env = Environment()
+            compile_wrapper = unix_path(self, os.path.join(self.source_folder, "build-aux", "compile"))
+            ar_wrapper = unix_path(self, self._user_info_build["automake"].ar_lib)
+            env.define("CC", f"{compile_wrapper} cl -nologo")
+            env.define("CXX", f"{compile_wrapper} cl -nologo")
+            env.define("LD", "link -nologo")
+            env.define("AR", f"{ar_wrapper} \"lib -nologo\"")
+            env.define("NM", "dumpbin -symbols")
+            env.define("OBJDUMP", ":")
+            env.define("RANLIB", ":")
+            env.define("STRIP", ":")
+            env.vars(self).save_script("conanbuild_libmodbus_msvc")
+
+    def _patch_sources(self):
+        apply_conandata_patches(self)
+        if not self.options.shared:
+            for decl in ("__declspec(dllexport)", "__declspec(dllimport)"):
+                replace_in_file(self, os.path.join(self.source_folder, "src", "modbus.h"), decl, "")
+
+    def build(self):
+        self._patch_sources()
+        autotools = Autotools(self)
+        autotools.configure(args=["--host=aarch64-rpi4-linux-musl"])
+        autotools.make()
+
+    def package(self):
+        copy(self, pattern="COPYING*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        autotools = Autotools(self)
+        # see https://github.com/conan-io/conan/issues/12006
+        autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        fix_apple_shared_install_name(self)
+        if is_msvc(self) and self.options.shared:
+            rename(self,
+                    os.path.join(self.package_folder, "lib", "modbus.dll.lib"),
+                    os.path.join(self.package_folder, "lib", "modbus.lib"))
+
+    def package_info(self):
+        self.cpp_info.set_property("pkg_config_name", "libmodbus")
+        self.cpp_info.libs = ["modbus"]
+        self.cpp_info.includedirs.append(os.path.join("include", "modbus"))
+        if self.settings.os == "Windows" and not self.options.shared:
+            self.cpp_info.system_libs = ["ws2_32"]
