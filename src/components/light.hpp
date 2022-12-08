62dd6bf23bdf466f8d136a5802b19c7d4f36d9d9
Rainer Poisel <rainer.poisel@gmail.com>
2022-12-08 09:44:04 +0100

    feat: light state logging incl name

--- src/components/light.hpp
+++ src/components/light.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
+#include <spdlog/spdlog.h>
+
+#include <string>
+
 namespace HomeAutomation {
 namespace Components {
 
 class Light {
 public:
-  Light() : state(false) {}
+  Light(std::string name) : name{name}, state(false) {
+    spdlog::info("Light {}: {}", name, state ? "on" : "off");
+  }
+  Light() : Light("") {}
 
   bool toggle() {
     state = !state;
+    spdlog::info("Light {}: {}", name, state ? "on" : "off");
     return state;
   }
   bool getState() const { return state; }
 
 private:
+  std::string name;
   bool state;
 };
 } // namespace Components
