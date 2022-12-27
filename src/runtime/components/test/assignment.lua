ddc7ed9675ac47b68194516b97ca9d4119daed3b
Rainer Poisel <rainer.poisel@gmail.com>
2022-12-27 13:43:42 +0100

    chore: remove support for Lua blocks

--- src/runtime/components/test/assignment.lua
+++
@@ -1,4 +0,0 @@
-gv:getInput('i'):set_int(gv:getInput('i'):as_int() + 1)
-gv:getInput('b'):set_bool(not gv:getInput('b'):as_bool())
-gv:getOutput('i'):set_int(gv:getOutput('i'):as_int() - 1)
-gv:getOutput('b'):set_bool(not gv:getOutput('b'):as_bool())
