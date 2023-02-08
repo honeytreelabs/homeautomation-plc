c6ef098318981fdc5611a44672e7280202bec33c
Rainer Poisel <rainer@honeytreelabs.com>
2023-02-08 19:18:34 +0100

    chore: make CI build cancelable

---
+++ docker/ci_logic.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+trap cleanup TERM INT EXIT
+cleanup() {
+    echo "Cleaning up ..."
+    docker compose rm -sf
+    for container_id in $(docker ps -a -f status=exited -q); do
+        docker rm "${container_id}"
+    done
+    docker network prune -f
+}
+
+get_script_dir() {
+    dirname "$(realpath ${BASH_SOURCE})"
+}
+
+exec_bg() {
+    "$@" &
+    wait $!
+}
+
+cd "$(get_script_dir)" || (echo "Could not change directory to $(get_script_dir)"; exit 1)
+
+case "${1}" in
+    run-build)
+        echo "Running build ..."
+        docker compose build
+        docker compose up -d --force-recreate build-env
+
+        # native platform
+        exec_bg docker compose exec -T -w / build-env make -f /source/Makefile native conandir=/prepare/build.venv sourcedir=/source
+        docker compose cp build-env:/build .
+        exec_bg tar czpf ../build.tgz build
+        # raspberry pi
+        exec_bg docker compose exec -T -w / build-env make -f /source/Makefile rpi3 conandir=/prepare/build.venv sourcedir=/source
+        ;;
+    run-test)
+        echo "Running test ..."
+        docker compose build
+        docker compose up -d --force-recreate
+
+        exec_bg tar xzf ../build.tgz
+        docker compose cp build build-env:/
+
+        exec_bg docker compose exec -T -w /source build-env make -f /source/Makefile test-nomemcheck testdir=/build
+        ;;
+esac
