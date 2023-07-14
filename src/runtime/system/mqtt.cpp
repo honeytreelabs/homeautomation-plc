8495e96e2239c4ff743fd76401a8b6ce8162709b
Rainer Poisel <rainer@honeytreelabs.com>
2023-07-14 22:50:16 +0200

    refactor: simplify payload handling in MQTT

--- src/runtime/system/mqtt.cpp
+++ src/runtime/system/mqtt.cpp
@@ -15,14 +15,7 @@ namespace IO {
 namespace MQTT {
 
 void Client::send(Topic const &topic, char const *payload) {
-  auto length = std::strlen(payload);
-  Payload payload_fwd;
-  payload_fwd.reserve(length);
-  for (std::size_t i = 0; i < length; i++) {
-    payload_fwd.push_back(static_cast<std::uint8_t>(payload[i]));
-  }
-  std::string topic_fwd{topic};
-  send(Message{topic_fwd, std::move(payload_fwd)});
+  send(Message{topic, Payload{payload, payload + std::strlen(payload)}});
 }
 void Client::send(Topic const &topic, std::string const &payload) {
   send(topic, payload.c_str());
