3e0d5cd0a7047539dce8e6718d6537a2d0286551
Rainer Poisel <rainer@embedded-focus.com>
2026-05-24 21:43:17 +0200

    dev: add Rust DevEnv using Nix flakes
    Signed-off-by: Rainer Poisel <rainer@embedded-focus.com>

---
+++ .nix/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Development environment for agent-circus";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          libPath = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib   # provides libstdc++.so.6 (and libgcc_s)
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # Encryption tools
+              age
+              sops
+
+              # Python
+              uv
+
+              # Rust
+              rust-analyzer
+
+              stdenv.cc.cc.lib
+            ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH=${libPath}:''${LD_LIBRARY_PATH:-}
+            '';
+          };
+        });
+    };
+}
